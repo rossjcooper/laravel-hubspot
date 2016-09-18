e8d150605ef1ec7f8167c8bc2f45b66fccac139b
Ross Cooper <therossjcooper@gmail.com>
2016-09-18 01:49:24 +0100

    Added facade

---
+++ src/Facades/HubSpot.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rossjcooper\LaravelHubSpot\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class HubSpot extends Facade
+{
+	/**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'Rossjcooper\LaravelHubSpot\HubSpot'; }
+}
