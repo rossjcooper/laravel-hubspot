2110b0bce79684519973861799468e5debbf747f
Ross Cooper <therossjcooper@gmail.com>
2017-05-06 23:53:52 +0100

    Removed wrong include for autoload file

--- src/HubSpotServiceProvider.php
+++ src/HubSpotServiceProvider.php
@@ -15,8 +15,6 @@ class HubSpotServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //Require our composer autoload.php file
-        require __DIR__ . '/../vendor/autoload.php';
         //Bind the HubSpot wrapper class
         $this->app->bind('Rossjcooper\LaravelHubSpot\HubSpot', function ($app) {
             return HubSpot::create(env('HUBSPOT_API_KEY'));
