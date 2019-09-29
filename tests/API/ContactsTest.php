932f74675b78f864baf76f46f8c3b5cc8e21a621
Ross Cooper <ross@craftaweb.co.uk>
2019-09-29 23:00:06 +0100

    Added and run PHP CS Fixer

--- tests/API/ContactsTest.php
+++ tests/API/ContactsTest.php
@@ -7,18 +7,17 @@ use Rossjcooper\LaravelHubSpot\HubSpot;
 
 class ContactsTest extends TestCase
 {
+    public function test_get_contacts()
+    {
+        $hubspot = app(HubSpot::class);
 
-	public function test_get_contacts()
-	{
-		$hubspot = app(HubSpot::class);
+        $response = $hubspot->contacts()->all();
 
-		$response = $hubspot->contacts()->all();
+        $contact = $response->contacts[0];
 
-		$contact = $response->contacts[0];
-
-		$this->assertIsArray($response->contacts);
-		// Test we have the default test contact
-		$this->assertEquals('Cool', $contact->properties->firstname->value);
-		$this->assertEquals('Robot (Sample Contact)', $contact->properties->lastname->value);
-	}
+        $this->assertIsArray($response->contacts);
+        // Test we have the default test contact
+        $this->assertEquals('Cool', $contact->properties->firstname->value);
+        $this->assertEquals('Robot (Sample Contact)', $contact->properties->lastname->value);
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,21 +8,21 @@ use Rossjcooper\LaravelHubSpot\HubSpotServiceProvider;
 
 abstract class TestCase extends OrchTestCase
 {
-	/**
-	 * Load our ServiceProvider
-	 */
-	protected function getPackageProviders($app)
-	{
-		return [HubSpotServiceProvider::class];
-	}
+    /**
+     * Load our ServiceProvider
+     */
+    protected function getPackageProviders($app)
+    {
+        return [HubSpotServiceProvider::class];
+    }
 
-	/**
-	 * Loads in our package .env file
-	 */
-	protected function getEnvironmentSetUp($app)
-	{
-		$app->useEnvironmentPath(__DIR__ . '/..');
-		$app->bootstrapWith([LoadEnvironmentVariables::class]);
-		parent::getEnvironmentSetUp($app);
-	}
+    /**
+     * Loads in our package .env file
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app->useEnvironmentPath(__DIR__ . '/..');
+        $app->bootstrapWith([LoadEnvironmentVariables::class]);
+        parent::getEnvironmentSetUp($app);
+    }
 }
