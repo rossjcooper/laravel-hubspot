3cb0b4a5af49899ac0a65948c389ee1fc4d2eea1
Ross Cooper <ross@craftaweb.co.uk>
2019-09-29 22:16:20 +0100

    Added test for API key being set correctly

--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -4,12 +4,21 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Rossjcooper\LaravelHubSpot\HubSpot;
+use SevenShores\Hubspot\Factory;
 
 class ServiceProviderTest extends TestCase
 {
-	public function test_service_provider_bindings() {
+	public function test_service_provider_bindings()
+	{
 		$hubspot = app(HubSpot::class);
 
 		$this->assertInstanceOf(HubSpot::class, $hubspot);
+	}
+	
+	public function test_api_key_set()
+	{
+		$hubspot = app(HubSpot::class);
+		
+		$this->assertEquals(env('HUBSPOT_API_KEY'), $hubspot->client->key);
 	}
 }
