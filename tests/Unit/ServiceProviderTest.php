9c76a95ea72f2849c2ca6ee182bd89ceed3b2fc3
Ross Cooper <ross@craftaweb.co.uk>
2019-09-29 23:07:36 +0100

    Added CS rules and travis script

--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -8,17 +8,17 @@ use SevenShores\Hubspot\Factory;
 
 class ServiceProviderTest extends TestCase
 {
-    public function test_service_provider_bindings()
-    {
-        $hubspot = app(HubSpot::class);
+	public function test_service_provider_bindings()
+	{
+		$hubspot = app(HubSpot::class);
 
-        $this->assertInstanceOf(HubSpot::class, $hubspot);
-    }
-    
-    public function test_api_key_set()
-    {
-        $hubspot = app(HubSpot::class);
-        
-        $this->assertEquals(env('HUBSPOT_API_KEY'), $hubspot->client->key);
-    }
+		$this->assertInstanceOf(HubSpot::class, $hubspot);
+	}
+	
+	public function test_api_key_set()
+	{
+		$hubspot = app(HubSpot::class);
+		
+		$this->assertEquals(env('HUBSPOT_API_KEY'), $hubspot->client->key);
+	}
 }
