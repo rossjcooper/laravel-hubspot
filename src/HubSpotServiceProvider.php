d812ce42bbcb51f47e4e2debbf1052e80ee0a6d7
Toby Evans <tobz.nz@gmail.com>
2020-06-11 16:23:07 +1200

    Setup handler for Hubspot's Rate Limiting
    This should automatically handle retries and such for 429 and 500 errors - which both occur quite a lot in my experience.
    This is taken pretty much straight from the Hubspot PHP SDK docs.
    
    There's a little BC issue with the `api_key` config - but I've included a fix for it, so no one should have any problems.

--- src/HubSpotServiceProvider.php
+++ src/HubSpotServiceProvider.php
@@ -3,32 +3,59 @@
 namespace Rossjcooper\LaravelHubSpot;
 
 use Illuminate\Support\ServiceProvider;
+use SevenShores\Hubspot\Delay;
+use SevenShores\Hubspot\RetryMiddlewareFactory;
 
 class HubSpotServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register the service provider.
-	 */
-	public function register()
-	{
-		//Bind the HubSpot wrapper class
-		$this->app->bind('Rossjcooper\LaravelHubSpot\HubSpot', function ($app) {
-			return HubSpot::create(
-				env('HUBSPOT_API_KEY', config('hubspot.api_key')),
-				null,
-				config('hubspot.client_options', [])
-			);
-		});
-	}
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        //Bind the HubSpot wrapper class
+        $this->app->bind('Rossjcooper\LaravelHubSpot\HubSpot', function ($app) {
+            $handlerStack = \GuzzleHttp\HandlerStack::create();
+            $handlerStack->push(
+                RetryMiddlewareFactory::createRateLimitMiddleware(
+                    Delay::getConstantDelayFunction()
+                )
+            );
 
-	/**
-	 * Perform post-registration booting of services.
-	 */
-	public function boot()
-	{
-		// config
-		$this->publishes([
-			__DIR__.'/config/hubspot.php' => config_path('hubspot.php'),
-		], 'config');
-	}
+            $handlerStack->push(
+                RetryMiddlewareFactory::createInternalErrorsMiddleware(
+                    Delay::getExponentialDelayFunction(2)
+                )
+            );
+
+            $guzzleClient = new \GuzzleHttp\Client(['handler' => $handlerStack]);
+
+            // BC fix
+            if (!config('hubspot.key')) {
+                config(['hubspot.key' => config('hubspot.api_key')]);
+            }
+
+            $httpClient = new \SevenShores\Hubspot\Http\Client(
+                config('hubspot'),
+                $guzzleClient,
+                config('hubspot.client_options', []),
+            );
+
+            return HubSpot::create(
+                env('HUBSPOT_API_KEY', config('hubspot.api_key')),
+                $httpClient
+            );
+        });
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     */
+    public function boot()
+    {
+        // config
+        $this->publishes([
+            __DIR__ . '/config/hubspot.php' => config_path('hubspot.php'),
+        ], 'config');
+    }
 }
