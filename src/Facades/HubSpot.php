18707ba17eb56fecaf040dc75e11dbf97812a558
Ross Cooper <ross@craftaweb.co.uk>
2023-06-26 17:59:16 +0100

    Updated facade

--- src/Facades/HubSpot.php
+++ src/Facades/HubSpot.php
@@ -13,6 +13,6 @@ class HubSpot extends Facade
 	 */
 	protected static function getFacadeAccessor()
 	{
-		return 'Rossjcooper\LaravelHubSpot\HubSpot';
+		return 'HubSpot\Discovery\Discovery';
 	}
 }
