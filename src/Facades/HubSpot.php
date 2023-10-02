3923c8c3a97b8e0fd262dac8edf1f487389d037b
Ross Cooper <ross@craftaweb.co.uk>
2023-10-02 21:02:18 +0100

    CS fix

--- src/Facades/HubSpot.php
+++ src/Facades/HubSpot.php
@@ -6,17 +6,17 @@ use HubSpot\Discovery\Discovery;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \HubSpot\Discovery\Auth\Discovery auth()
- * @method static \HubSpot\Discovery\Automation\Discovery automation()
- * @method static \HubSpot\Discovery\Cms\Discovery cms()
- * @method static \HubSpot\Discovery\Conversations\Discovery conversations()
+ * @method static \HubSpot\Discovery\Auth\Discovery                     auth()
+ * @method static \HubSpot\Discovery\Automation\Discovery               automation()
+ * @method static \HubSpot\Discovery\Cms\Discovery                      cms()
+ * @method static \HubSpot\Discovery\Conversations\Discovery            conversations()
  * @method static \HubSpot\Discovery\CommunicationPreferences\Discovery communicationPreferences()
- * @method static \HubSpot\Discovery\Crm\Discovery crm()
- * @method static \HubSpot\Discovery\Events\Discovery events()
- * @method static \HubSpot\Discovery\Files\Discovery files()
- * @method static \HubSpot\Discovery\Marketing\Discovery marketing()
- * @method static \HubSpot\Discovery\Settings\Discovery settings()
- * @method static \HubSpot\Discovery\Webhooks\Discovery webhooks()
+ * @method static \HubSpot\Discovery\Crm\Discovery                      crm()
+ * @method static \HubSpot\Discovery\Events\Discovery                   events()
+ * @method static \HubSpot\Discovery\Files\Discovery                    files()
+ * @method static \HubSpot\Discovery\Marketing\Discovery                marketing()
+ * @method static \HubSpot\Discovery\Settings\Discovery                 settings()
+ * @method static \HubSpot\Discovery\Webhooks\Discovery                 webhooks()
  */
 class HubSpot extends Facade
 {
